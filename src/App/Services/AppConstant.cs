5fb636708536e9177fea7f502a0bcbce61694fe6
Claudio Sanchez <claudio@megsoftconsulting,com>
2019-11-30 03:54:05 -0500

    Step 2 Implemented a FeatureManager class

--- src/App/Services/AppConstant.cs
+++ src/App/Services/AppConstant.cs
@@ -18,5 +18,7 @@ namespace App.Services
 
         public static readonly Color Background = Color.FromHex("F8F8F8");
 
+        public static readonly string LaunchDarklyKey = "mob-257ecb8d-5bc8-43db-bd87-d1089b590167";
+        public static readonly string UserKey = "claudio-sanchez";
     }
 }
--- src/App/Services/FeatureManager.cs
+++ src/App/Services/FeatureManager.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿
+using System;
+using LaunchDarkly.Client;
+using LaunchDarkly.Xamarin;
+
 namespace App.Services
 {
-    public class FeatureManager
+    public static class Features
     {
-        public FeatureManager()
+        public static readonly string CanShowCategoryTags = "show-category-tags";
+    }
+
+    public static class FeatureManager
+    {
+        public static LdClient FeatureFlagClient;
+        private static bool isInialized;
+
+        public  static void Init()
         {
+            if (isInialized) return;
+
+            var user = User.WithKey(AppConstant.UserKey);
+            var timeSpan = TimeSpan.FromSeconds(5);
+            FeatureFlagClient = LdClient.Init(AppConstant.LaunchDarklyKey, user, timeSpan);
+            isInialized = true;
+        }
+
+        public static bool IsEnabled(string slug)
+        {
+            if (!isInialized) Init();
+            return FeatureFlagClient.BoolVariation(slug);
         }
     }
 }
